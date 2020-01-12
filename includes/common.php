eb1639e9efaf22da6517f887f3c813d0f6c94809
Jeremy Felt <jeremy.felt@gmail.com>
2020-01-11 16:40:18 -0800

    Remove actions for wlwmanifest and shortlink output

--- includes/common.php
+++ includes/common.php
@@ -2,7 +2,8 @@
 
 namespace JWC\Common;
 
-add_action( 'plugin_loaded', __NAMESPACE__ . '\remove_extra_emoji_handling' );
+add_action( 'plugins_loaded', __NAMESPACE__ . '\remove_extra_emoji_handling' );
+add_action( 'plugins_loaded', __NAMESPACE__ . '\remove_default_actions' );
 
 /**
  * Free emoji to be exactly what they are and remain optimistic that the
@@ -24,6 +25,21 @@ function remove_extra_emoji_handling() {
 	remove_filter( 'the_content_feed', 'wp_staticize_emoji' );
 	remove_filter( 'comment_text_rss', 'wp_staticize_emoji' );
 	remove_filter( 'wp_mail', 'wp_staticize_emoji_for_email' );
+}
+
+/**
+ * Remove other default actions that output unnecessary elements or do
+ * unnecessary things.
+ */
+function remove_default_actions() {
+
+	// There is a very close to zero chance I will ever use Windows Live Writer.
+	remove_action( 'wp_head', 'wlwmanifest_link' );
+
+	// I can't ever imagine wanting a shortlink in today's internet, but that may
+	// just be me.
+	remove_action( 'wp_head', 'wp_shortlink_wp_head', 10, 0 );
+	remove_action( 'template_redirect', 'wp_shortlink_header', 11, 0 );
 }
 
 add_filter( 'wp_resource_hints', __NAMESPACE__ . '\remove_wp_org_cdn_prefetch' );
